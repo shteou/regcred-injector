e0bcda0694e63a7b007def92b6162a4d5f42fe5a
Stewart Platt <shteou@gmail.com>
2020-11-14 22:31:51 +0000

    Use portt 8443 in preparaiton of TLS support

--- main.go
+++ main.go
@@ -74,7 +74,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "127.0.0.1:8080",
+		Addr:         "127.0.0.1:8443",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
