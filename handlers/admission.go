3fcbbb354c9ec70fac29272fb22250c46c98fd11
Stewart Platt <shteou@gmail.com>
2020-11-16 16:43:54 +0000

    Make logging consistent (#19)
    Made logging more consistent, with the UID logged at every step,
    and added several new log entries

--- handlers/admission.go
+++ handlers/admission.go
@@ -12,6 +12,7 @@ import (
 	admission "k8s.io/api/admission/v1beta1"
 	apiv1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -30,7 +31,7 @@ func getReview(r *http.Request) (admission.AdmissionReview, error) {
 	return rev, nil
 }
 
-func createSecret(namespace string) error {
+func createSecret(namespace string, uid types.UID) error {
 	secrets, err := Clientset.CoreV1().Secrets(namespace).List(context.TODO(), v1.ListOptions{})
 	if err != nil {
 		return err
@@ -44,6 +45,8 @@ func createSecret(namespace string) error {
 	}
 
 	if hasSecret == false {
+		log.Printf("%s: creating credentials in %s", uid, namespace)
+
 		secret := apiv1.Secret{}
 		secret.Type = "kubernetes.io/dockerconfigjson"
 		secret.Name = "regcred"
@@ -64,8 +67,12 @@ func createSecret(namespace string) error {
 		secret.Data[".dockerconfigjson"] = []byte(dockerConfigJSON)
 		_, err = Clientset.CoreV1().Secrets(namespace).Create(context.TODO(), &secret, v1.CreateOptions{})
 		if err != nil {
+			log.Printf("%s: credential creation in %s faied", uid, namespace)
 			return err
 		}
+		log.Printf("%s: credential creation in %s succeeded", uid, namespace)
+	} else {
+		log.Printf("%s: skipping credentials in %s, already exists", uid, namespace)
 	}
 	return nil
 }
@@ -80,7 +87,7 @@ func generateResponseReview(req admission.AdmissionReview, mutate bool) (*admiss
 	responseReview.Response.Allowed = true
 
 	if mutate == true {
-		log.Println("Mutating")
+		log.Printf("%s: Mutating pod with imagePullSecrets", req.Request.UID)
 		patchType := admission.PatchTypeJSONPatch
 		responseReview.Response.PatchType = &patchType
 		patch, err := json.Marshal(generatePatchResponse())
@@ -111,61 +118,58 @@ func PodHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Printf("Received a webhook request with UID %s", req.Request.UID)
+	log.Printf("%s: received a webhook request", req.Request.UID)
 
 	podObject, err := req.Request.Object.MarshalJSON()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	log.Println(string(podObject))
+
 	var pod apiv1.Pod
 	err = json.Unmarshal(podObject, &pod)
 	if err != nil {
-		log.Printf("Failed to unmarshal pod JSON into pod object for pod %s", req.Request.UID)
+		log.Printf("%s: failed to unmarshal pod JSON into pod object", req.Request.UID)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	log.Printf("Identified target pod %s in namespace %s for UID %s", pod.ObjectMeta.Name, pod.ObjectMeta.Namespace, req.Request.UID)
+	log.Printf("%s: Identified type %s (%s) in namespace %s", req.Request.UID, pod.Kind, pod.ObjectMeta.Name, pod.ObjectMeta.Namespace)
 
 	namespace := pod.ObjectMeta.Namespace
 	if namespace == "" {
 		namespace = pod.Namespace
 	}
-	log.Printf("Namespace was: %s", namespace)
+	log.Printf("%s: namespace determined: %s", req.Request.UID, namespace)
 	if namespace != "" {
-		log.Printf("Creating secret in namespace %s", pod.Namespace)
-		err = createSecret(pod.ObjectMeta.Namespace)
+		err = createSecret(pod.ObjectMeta.Namespace, req.Request.UID)
 		if err != nil {
-			log.Printf("Encountered an error creating the secret in %s: %s", pod.ObjectMeta.Namespace, err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		log.Printf("Created secret in namespace %s for UID %s", pod.ObjectMeta.Namespace, req.Request.UID)
 	}
 
 	var responseReview *admission.AdmissionReview
 	if pod.Kind == "Pod" {
-		log.Println("Found a Pod, going to generate a patch response")
+		log.Printf("%s: generating patch response for pod", req.Request.UID)
 		responseReview, err = generateResponseReview(req, true)
 	} else {
 		responseReview, err = generateResponseReview(req, false)
 	}
 
 	if err != nil {
-		log.Printf("Failed to generate response Review for UID %s", req.Request.UID)
+		log.Printf("%s: failed to generate response Review", req.Request.UID)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	responseBytes, err := json.Marshal(responseReview)
 	if err != nil {
-		log.Printf("Failed to marshal response Review to bytes for UID %s", req.Request.UID)
+		log.Printf("%s: failed to marshal response Review to bytes", req.Request.UID)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	log.Printf("Successfully serviced request %s", req.Request.UID)
+	log.Printf("%s: request successful", req.Request.UID)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(responseBytes)
